71e4558a1adcce93b26d5c8c83e21e3ccd1f7fc9
JooHyun <july5087@gmail.com>
2020-06-17 13:15:44 +0900

    구글 로그인 연동

--- src/main/java/com/example/book/springboot/Application.java
+++ src/main/java/com/example/book/springboot/Application.java
@@ -7,7 +7,5 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 @EnableJpaAuditing //JPA Auditing 활성화
 @SpringBootApplication
 public class Application {
-    public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
-    }
+    public static void main(String[] args) { SpringApplication.run(Application.class, args); }
 }
--- src/main/java/com/example/book/springboot/web/IndexController.java
+++ src/main/java/com/example/book/springboot/web/IndexController.java
@@ -2,21 +2,29 @@ package com.example.book.springboot.web;
 
 import com.example.book.springboot.service.posts.PostsService;
 import com.example.book.springboot.web.dto.PostsResponseDto;
+import com.example.book.springboot.web.dto.SessionUser;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import javax.servlet.http.HttpSession;
+
 @RequiredArgsConstructor
 @Controller
 public class IndexController {
 
     private final PostsService postsService;
+    private final HttpSession httpSession;
 
     @GetMapping("/")
     public String index(Model model){
         model.addAttribute("posts", postsService.findAllDesc());
+        SessionUser user = (SessionUser) httpSession.getAttribute("user");
+        if(user != null){
+            model.addAttribute("userName", user.getName());
+        }
         return "index";
     }
 
