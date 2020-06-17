9b74a673b76ba7618adb2d91df23bce027a30cb0
JooHyun <july5087@gmail.com>
2020-06-17 13:53:43 +0900

    @LoginUser 어노테이션 기반으로 개선

--- src/main/java/com/example/book/springboot/web/IndexController.java
+++ src/main/java/com/example/book/springboot/web/IndexController.java
@@ -1,28 +1,25 @@
 package com.example.book.springboot.web;
 
+import com.example.book.springboot.config.auth.LoginUser;
 import com.example.book.springboot.service.posts.PostsService;
 import com.example.book.springboot.web.dto.PostsResponseDto;
-import com.example.book.springboot.web.dto.SessionUser;
+import com.example.book.springboot.config.auth.dto.SessionUser;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-import javax.servlet.http.HttpSession;
-
 @RequiredArgsConstructor
 @Controller
 public class IndexController {
 
     private final PostsService postsService;
-    private final HttpSession httpSession;
 
     @GetMapping("/")
-    public String index(Model model){
+    public String index(Model model, @LoginUser SessionUser user) {
         model.addAttribute("posts", postsService.findAllDesc());
-        SessionUser user = (SessionUser) httpSession.getAttribute("user");
-        if(user != null){
+        if (user != null) {
             model.addAttribute("userName", user.getName());
         }
         return "index";
