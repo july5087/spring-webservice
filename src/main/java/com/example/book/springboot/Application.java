0cd785890ba42c4fec1532c43d0d63b3377ec141
JooHyun <july5087@gmail.com>
2020-06-18 15:53:47 +0900

    update

--- src/main/java/com/example/book/springboot/Application.java
+++ src/main/java/com/example/book/springboot/Application.java
@@ -2,9 +2,8 @@ package com.example.book.springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-//@EnableJpaAuditingr가 삭제됨
+//@EnableJpaAuditing가 삭제됨
 @SpringBootApplication
 public class Application {
     public static void main(String[] args) { SpringApplication.run(Application.class, args); }
